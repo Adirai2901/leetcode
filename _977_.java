99d965cb8ca2b909dee22b7ad2e1b4dc5bf0a2ff
Aditya Rai <adirai2901@gmail.com>
2025-12-02 21:56:40 +0530

    Leetcode

---
+++ _977_.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class _977_ {
+    class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length;i++){
+            nums[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+        
+    }
+}
+    
+}
