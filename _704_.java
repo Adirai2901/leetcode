980da4c6a653131036eae1abe0932d23b5041c81
Aditya Rai <adirai2901@gmail.com>
2025-12-04 21:55:57 +0530

    Leetcode

---
+++ _704_.java
@@ -0,0 +1,21 @@
+public class _704_ {
+    class Solution {
+    public int search(int[] nums, int target) {
+        int i =0; 
+        int j =nums.length-1;
+        while(j>i){
+            int mid = (i+j)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(target>nums[mid]){
+                i = mid+1;
+            }
+            if(target<nums[mid]){
+                j=mid-1;
+            }
+        }
+        return -1;
+    }
+}
+}
