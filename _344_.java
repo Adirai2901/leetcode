29bdf3703ae0dbbb92fe28c3bf6bc9d6bc61d6c5
Aditya Rai <adirai2901@gmail.com>
2025-12-04 21:49:09 +0530

    Leetcode

---
+++ _344_.java
@@ -0,0 +1,18 @@
+public class _344_ {
+    class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+
+        while(end>start){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end]=temp;
+            start++;
+            end--;
+        }
+
+        
+    }
+}
+}
